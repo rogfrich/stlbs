5ef81a014f3e8b3c67df9ae9c7e5fc02fb351a77
Rich <richard.a.cairns@gmail.com>
2022-06-08 11:37:13 +0100

    basic implementation of StLB

---
+++ tests/test_all.py
@@ -0,0 +1,56 @@
+def test_stlb_class_is_importable():
+    """
+    Given that the StLbs package is installed
+    as a user of the package
+    I want to be able to import it.
+    """
+    from stlbs import StLb
+
+def test_input_weight_correctly_assigned():
+    """
+    Given that I have created an instance of StLb and passed it the correct parameters for stones and lbs
+    as a user of the package
+    I want the values of instance.whole_stones and instance.remainder_lbs to be correct
+    """
+    from stlbs import StLb
+    x = StLb([10, 1])
+    assert x.whole_stones == 10
+    assert x.remainder_lbs == 1
+
+def test_in_lbs():
+    """
+    Given that I have created an instance of StLb and passed it the correct parameters for stones and lbs
+    as a user of the package
+    I want the value of instance.in_lbs to be (stones * 14) + remainder lbs
+    """
+    from stlbs import StLb
+    test1 = StLb([10, 1])
+    assert test1.in_lbs == 141
+    test2 = StLb([0, 4])
+    assert test2.in_lbs == 4
+    test3 = StLb([1,0])
+    assert test3.in_lbs == 14
+    test4 = StLb([0, 281])
+    assert test4.in_lbs == 281
+
+def test_weight_in_lbs_only():
+    """
+    Given that an instance of StLb is instantiated with a "lbs only" value >= 14 (i.e [0, 280])
+    as a user of the package
+    I want the "lbs only" value to be correctly converted into stones and lbs and whole_stones and remainder_lbs to be set
+    """
+    from stlbs import StLb
+    test1 = StLb([0, 281])  # 20 stones and 1 lb
+    assert test1.whole_stones == 20
+    assert test1.remainder_lbs == 1
+    assert test1.in_lbs == 281
+
+    test2 = StLb([0, 15])
+    assert test2.whole_stones == 1
+    assert test2.remainder_lbs == 1
+    assert test2.in_lbs == 15
+
+    test3 = StLb([0, 14])
+    assert test3.whole_stones == 1
+    assert test3.remainder_lbs == 0
+    assert test3.in_lbs == 14
